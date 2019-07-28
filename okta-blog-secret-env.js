6bd0d6db1ea7ba4b9dc0560fe3249b5b2bc4da42
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2019-07-28 11:46:34 -0700

    Update okta-blog-secret-env.js

--- okta-blog-secret-env.js
+++ okta-blog-secret-env.js
@@ -1,5 +1,8 @@
+//NOTE: this file was commited to github. credentials might have been compromised.
+//Reset credentials before using it
+
 OKTA_ORG_URL=https://dev-772205.okta.com 
-OKTA_CLIENT_ID=0oafzczf4s6RNWExM356
-OKTA_CLIENT_SECRET=BtGspGlRoEMsNEEbONJvVkSGUj8duH4n__p7NbOd
+OKTA_CLIENT_ID=xxxf4s6RNWExM356
+OKTA_CLIENT_SECRET=xxxJvVkSGUj8duH4n__p7NbOd
 REDIRECT_URL=http://localhost:3000/authorization-code/callback 
-RANDOM_SECRET_WORD='super secret' 
+RANDOM_SECRET_WORD='xxx xxx xxx' 
